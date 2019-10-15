0b716844d763c0dfd7ca26bbe2fcd9d1438af12a
sammychinedu2ky <sammychinedu2ky@gmail.com>
2019-10-15 12:26:06 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sammychinedu2ky/FormManagement.git/precache-manifest.4d43908af7a60566ec38c55de5756c57.js"
+  "/sammychinedu2ky/FormManagement2.git/precache-manifest.dde760828a6b5259063835dc54fdda29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sammychinedu2ky/FormManagement.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sammychinedu2ky/FormManagement2.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
