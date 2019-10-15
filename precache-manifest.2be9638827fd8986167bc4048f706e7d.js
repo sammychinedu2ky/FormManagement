15329adbdb7165de6014608fe1bbbba68a735439
sammychinedu2ky <sammychinedu2ky@gmail.com>
2019-10-15 12:39:57 +0100

    Updates

---
+++ precache-manifest.2be9638827fd8986167bc4048f706e7d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b80f54ec2061c2b20d3f4bde25a7f51",
+    "url": "/sammychinedu2ky/FormManagement/index.html"
+  },
+  {
+    "revision": "d0f9289c17dfda7664ff",
+    "url": "/sammychinedu2ky/FormManagement/static/js/2.41dc4622.chunk.js"
+  },
+  {
+    "revision": "705ce572310f66441349",
+    "url": "/sammychinedu2ky/FormManagement/static/js/main.ee564c88.chunk.js"
+  },
+  {
+    "revision": "19d93c31f4775741f9bf",
+    "url": "/sammychinedu2ky/FormManagement/static/js/runtime-main.3875cb55.js"
+  }
+]);
